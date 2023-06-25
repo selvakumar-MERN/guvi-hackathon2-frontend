8f1418301930bfd7532886b46f53c581985ef3a0
selvakumar k <84513538+selvakumar-MERN@users.noreply.github.com>
2023-06-25 20:28:15 +0530

    Update Products.js

--- src/Components/Productpage/Products.js
+++ src/Components/Productpage/Products.js
@@ -10,6 +10,7 @@ import Footer from '../Footer/Footer';
 
 
 function Products(props) {
+    const [loading,setloading]=useState(false)
     const notify = () => toast("Added to Cart");
     const [product, setproduct] = useState([])
     const [records, setrecords] = useState([])
@@ -22,7 +23,7 @@ function Products(props) {
         axios.get('https://guvi-hackathon2-6k8d.onrender.com/product/products')
             .then(res => {
 
-
+                 setloading(true)
                 setproduct(res.data)
                 setrecords(res.data)
             })
@@ -61,7 +62,7 @@ function Products(props) {
                 </div>
 
                 <h2 className='mt-4'>Products</h2>
-                <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 mt-4 ">
+                { loading ?   <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 mt-4 ">
 
 
                     {records.map((items) =>
@@ -93,7 +94,13 @@ function Products(props) {
                         </div>
 
                     )}
+                </div> : <section >
+                    <div className='container mt-3 text-center'> 
+                 <div className='spinner-border text-primary ' role='status'>
+                <span className='sr-only'>Loading...</span>
                 </div>
+                </div>
+                </section>}
 
 
             </div>
