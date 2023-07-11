2867a3fdb17d4075612f713ce9ab6366ae74bfe1
selvakumar k <84513538+selvakumar-MERN@users.noreply.github.com>
2023-07-11 18:33:22 +0530

    Update Cart.js

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,39 +1,61 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Usernav from '../Landingpage/Usernav';
 import axios from 'axios';
 import Footer from '../Footer/Footer';
+import Mycontext from '../../Context';
+import Calender from '../Calender';
 
 
 
 function Cart(props) {
-    
-    const [users, setusers] = useState({})
-    const [cartdetails, setcart] = useState(props.cart)
 
+    const [amount, setamount] = useState('')
+    const { CartItems, setCartItems } = useContext(Mycontext)
+    const user = {
+        userName: "",
+        email: "",
+        data: CartItems
+    }
+    const [users, setusers] = useState(user)
 
-    const handlechange = (e) => {
+    //increase quantity
+    const increase = (id) => {
+        const find = CartItems.findIndex((value) => value[0]._id === id)
+        if (CartItems[find][0].quantity !== 5) {
+            CartItems[find][0].quantity += 1
+        }
+        setCartItems([...CartItems])
+    }
+
+    //decrease quantity
+    const decrease = (id) => {
+        const find = CartItems.findIndex((value) => value[0]._id === id)
+        if (CartItems[find][0].quantity !== 1) {
+            CartItems[find][0].quantity -= 1
+        }
+        setCartItems([...CartItems])
+    }
+
+    //remove cart item
+    const removeitem = (id) => {
+        const find = CartItems.findIndex((value) => value[0]._id === id)
+        CartItems.splice(find, 1)
+        setCartItems([...CartItems])
+    }
+
+    //total amount
+    const total = CartItems.reduce((a, b) => {
+        return (a = a + b[0].productPrice * b[0].quantity * b[0].hours);
+
+    }, 0);
+
+    //handling email and name
+    const handler = (e) => {
         const { name, value } = e.target
         setusers({ ...users, [name]: value })
-        console.log(users)
+
     }
-    // changing the price while changing quantitty 
-    const handleprice = (e) => {
-        const name = "productPrice";
-        if (e.target.value === "") {
-            const value = cartdetails.productPrice * 0;
-            setcart({ ...cartdetails, [name]: value })
-        }
-        else if (e.target.value === "1") {
-            const value = props.cart.productPrice;
-            setcart({ ...cartdetails, [name]: value })
-            console.log(cartdetails)
-        }
-        else {
-            const value = props.cart.productPrice * e.target.value;
-            setcart({ ...cartdetails, [name]: value })
-            console.log(cartdetails)
-        }
-    }
+
     //razor pay payment handler
     const handleRazorpay = (data) => {
         const options = {
@@ -46,14 +68,14 @@ function Cart(props) {
             handler: function (response) {
                 console.log(response)
 
-                axios.post('https://guvi-hackathon2-6k8d.onrender.com/product/verify', { response: response })
+                axios.post('http://localhost:3030/product/verify', { response: response })
                     .then(res => {
 
                         //signature verification and creating the customer record            
                         if (res.data.message === 'signature valid') {
-                            axios.post('https://guvi-hackathon2-6k8d.onrender.com/user/createUser', users)
+                            axios.post('http://localhost:3030/user/createUser', users)
                                 .then(res => {
-                                    setcart("")
+                                    //  setcart("")
                                     return res
                                 })
                                 .catch(error => {
@@ -71,12 +93,12 @@ function Cart(props) {
         const rzp1 = new window.Razorpay(options);
         rzp1.open()
     }
-    const handlepayment = (e, amount) => {
+    const handlepayment = (e) => {
         e.preventDefault()
-
+        console.log(CartItems)
         const data = { amount: amount }
 
-        axios.post('https://guvi-hackathon2-6k8d.onrender.com/product/payment', data)
+        axios.post('http://localhost:3030/product/payment', data)
             .then(res => {
                 handleRazorpay(res.data.data)
             })
@@ -87,97 +109,50 @@ function Cart(props) {
     return (
         <div>
             <Usernav />
-            <div className='container mt-4'>
+            <div className='container'>
                 <div className='row'>
-                    <h2>Cart</h2>
+                    <h2 className='mt-2'>Cart</h2>
                 </div>
+                {CartItems.length > 0 ? <div>
+                    <table className='table table-responsive-sm'>
+                        <thead>
+                            <tr>
+                                <th>Product Name</th>
+                                <th>Product Price</th>
+                                <th>Product Quantity</th>
+                                <th>Date</th>
+                                <th>hours</th>
+                                <th>Total Amount</th>
+                                <th>Remove from cart</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
-               { cartdetails!=="" ? <div className='row'>
-              
-                   <div className='col-12 col-md-6 mt-3' >
-                           <h5>Product Details</h5>
-                           <div className='row'>
-                            <div className='col-12 col-md-8'>
-                            <div className="card h-100">
-                                {/*Product image*/}
-                                <img className="card-img-top" src={cartdetails.productImage} alt="..." height={"200px"} width={"100px"}></img>
-                                { /*Product details*/}
-                                <div className="card-body p-2">
-                                    <div className="text-left">
-                                        {/*Product name*/}
-                                        <h6 className="fw-bolder">{cartdetails.productName}</h6>
-                                        {/*Product price*/}
-                                        <div className="text-decoration-line-through">Rs.{cartdetails.productPrice}</div>
+                            {CartItems.map(items =>
+                                <tr>
+                                    <td className='d-flex'><img src={items[0].productImage} height={"100px"} width={"100px"} alt='...'></img><span className='ml-2'>{items[0].productName}</span></td>
+                                    <td>{items[0].productPrice}</td>
+                                    <td className='d-flex-column'><button onClick={() => { decrease(items[0]._id) }} style={{ border: "none", backgroundColor: "orange" }}>-</button><span>{items[0].quantity}</span><button onClick={() => { increase(items[0]._id) }} style={{ border: "none", backgroundColor: "orange" }}>+</button></td>
+                                    <td ><Calender id={items[0]._id} /></td>
+                                    <td>{items[0].hours}</td>
+                                    <td>{items[0].productPrice * items[0].quantity * items[0].hours}</td>
+                                    <td><button className='btn btn-danger' onClick={() => { removeitem(items[0]._id) }}>Remove</button></td>
 
-                                    </div>
-                                </div>
-                                </div>
-                            </div>
+                                </tr>)}
+                        </tbody>
 
-                           </div>
+                    </table>
+                    <h3>Total Amount : {total}</h3>
+                    <form className='col-12 col-md-6 form-group'>
                         <div>
-                            <h6 className='mt-3'>Fill Out the form and Check out we will contact you about further details or you can find our 
-                                mobile number in contact us page
-                            </h6>
+                            <input type='text' name='userName' placeholder='name' onChange={handler} className='form-control mb-2'></input>
                         </div>
                         <div>
-                            <h6>Keep the payment recipt safe for further verification</h6>
+                            <input type='email' name='email' placeholder='email' onChange={handler} className='form-control'></input>
                         </div>
-
-                    </div>
-                    <div className='col-12 col-md-6' >
-                        <form>
-                            <div className='form-group'>
-                                <input type='text' className='form-control' name='rentedItem' onChange={handlechange} placeholder='Product Name' required></input>
-                            </div>
-                            <div className='form-group'>
-                                <input type='text' className='form-control' name='name' placeholder='Name' onChange={handlechange} required></input>
-                            </div>
-                            <div className='form-group'>
-                                <input type='text' className='form-control' name='email' placeholder='email' onChange={handlechange} required></input>
-                            </div>
-                            <div className='form-group'>
-                                <input type='text' className='form-control' name='quantity' placeholder='quantity' required onChange={(e) => { handlechange(e); handleprice(e) }}></input>
-                            </div>
-                            <div className='row'>
-                                <div className='col-12 col-md-6'>
-                                    <div className='form-group'>
-                                        <label>Start Date</label>
-                                        <input type='date' className='form-control' name='fromDate' required onChange={handlechange}></input>
-                                    </div>
-                                </div>
-                                <div className='col-12 col-md-6'>
-                                    <div className='form-group'>
-                                        <label>Start Time</label>
-                                        <input type='time' className='form-control' name='fromTime' required onChange={handlechange}></input>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='row'>
-                                <div className='col-12 col-md-6'>
-                                    <div className='form-group'>
-                                        <label>End Date</label>
-                                        <input type='date' className='form-control' name='toDate' required onChange={handlechange}></input>
-                                    </div>
-                                </div>
-                                <div className='col-12 col-md-6'>
-                                    <div className='form-group'>
-                                        <label>End Time</label>
-                                        <input type='time' className='form-control' name='toTime' required onChange={handlechange}></input>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <button className='btn btn-warning' onClick={(e) => handlepayment(e, cartdetails.productPrice)}>Checkout</button>
-                        </form>
-
-
-
-
-                    </div>
-                </div>: 
-                <div className='text-center'>
-                    <h4>No items in the cart ! Please Add items</h4></div>} 
+                    </form>
+                    <button className='btn btn-warning' onClick={(e) => handlepayment(e, setamount(total))}>Check out</button>
+                </div> : <h4>Cart is Empty</h4>}
             </div>
             <div className='my-5'>
                 <Footer />
